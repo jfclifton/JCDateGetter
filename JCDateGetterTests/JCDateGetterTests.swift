f90827b68d30198b21c33078bffa3c7899b3ae50
Morgan <gormster@me.com>
2014-06-05 18:33:43 +1000

    added tests for days and weeks

--- JCDateGetterTests/JCDateGetterTests.swift
+++ JCDateGetterTests/JCDateGetterTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import JCDateGetter
 
 class JCDateGetterTests: XCTestCase {
     
@@ -20,16 +21,50 @@ class JCDateGetterTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testDays() {
+        var d1 = NSDate()
+        d1 = d1.dateByAddingTimeInterval(-259200)
+        
+        var d2 = 3.days(.ago)
+        
+        let t12 = d1.timeIntervalSinceDate(d2)
+        
+        XCTAssert(t12 < 1.0, "Days ago failed; difference was \(t12) seconds")
+        
+        var d3 = NSDate()
+        d3 = d3.dateByAddingTimeInterval(432000)
+        
+        var d4 = 5.days(.ahead)
+        
+        let t34 = d3.timeIntervalSinceDate(d4)
+        
+        XCTAssert(t34 < 1.0, "Days ago failed; difference was \(t34) seconds")
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testWeeks() {
+        var d1 = NSDate()
+        d1 = d1.dateByAddingTimeInterval(-1814400)
+        
+        var d2 = 3.weeks(.ago)
+        
+        let t12 = d1.timeIntervalSinceDate(d2)
+        
+        XCTAssert(t12 < 1.0, "Days ago failed; difference was \(t12) seconds")
+        
+        var d3 = NSDate()
+        d3 = d3.dateByAddingTimeInterval(3024000)
+        
+        var d4 = 5.weeks(.ahead)
+        
+        let t34 = d3.timeIntervalSinceDate(d4)
+        
+        XCTAssert(t34 < 1.0, "Days ago failed; difference was \(t34) seconds")
+        
+    }
+    
+    func testMonths() {
+        // I can't think of a good way to test this
     }
     
 }
